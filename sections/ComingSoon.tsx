42ee206bccd8e1fcd81df8fcbe2fb1efe5a82269
Eduardo Brito <eduardo.guardian@gmail.com>
2025-09-18 12:58:50 -0300

    Add ComingSoon Section

---
+++ sections/ComingSoon.tsx
@@ -0,0 +1,29 @@
+import Icon from "site/components/ui/Icon.tsx";
+
+export interface Props {
+  title: string;
+  description?: string;
+  position?: "flex-col" | "flex-col-reverse";
+}
+
+export default function ComingSoon(
+  { title, description, position = "flex-col" }: Props,
+) {
+  return (
+    <div
+      class={`flex ${position} gap-6 items-center justify-center w-full h-dvh bg-black text-white px-6 font-futura`}
+    >
+      <Icon
+        name={"ZeeDogHorizontalConnecting"}
+        width={152}
+        height={55}
+        fill="#ffffff"
+      />
+
+      <div class="flex flex-col gap-2.5 items-center justify-center text-center w-full font-futura mt-8">
+        <h1 class="text-3xl font-bold">{title}</h1>
+        {description && <span class="text-base lg:text-lg">{description}</span>}
+      </div>
+    </div>
+  );
+}
